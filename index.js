f4287c25578a1e4b16a2e4e15deb105a0ebb0433
Hadyaziz05 <hadyabdelaziz05@gmail.com>
2024-04-16 21:42:37 +0200

    Made webpage simpler

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get("/login", function (req, res) {
 app.get("/home", function (req, res) {
     res.sendFile(__dirname + "/public/HTML/home.html");
 });
+
+app.get("/template", function (req, res) {
+    res.sendFile(__dirname + "/public/HTML/template.html");
+});
 app.listen(3002, function () {
     console.log("Server is running on localhost3002");
 });
