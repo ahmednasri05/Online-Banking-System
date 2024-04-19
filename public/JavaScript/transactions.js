8f317a6c40210b10ee0cc299980b19d094cd3bd8
BimaTh <61094545+BimaTh@users.noreply.github.com>
2024-04-19 18:53:25 +0200

    Sessions Finished and Auth

--- public/JavaScript/transactions.js
+++ public/JavaScript/transactions.js
@@ -1,26 +1,44 @@
-function transfer() {
-var receiver = document.getElementById("receiver").value;
-var amount = document.getElementById("amount").value;
-const myHeaders = new Headers();
-myHeaders.append("x-auth-token", localStorage.getItem('token'));
-myHeaders.append("Content-Type", "application/json");
+const express = require("express");
 
-const raw = JSON.stringify({
-  "account": receiver,
-  "TransactionAmount": amount
-});
 
-const requestOptions = {
-  method: "POST",
-  headers: myHeaders,
-  body: raw,
-  redirect: "follow"
-};
+async function transfer(app) {
+  app.post("/transactions", async (res, req) => {
+    app.use(express.urlencoded({ extended: true }))
+    console.log(req.body)
+    const { receiver, amount } = req.body;
+    const myHeaders = new Headers();
+    myHeaders.append("x-auth-token", localStorage.getItem("token"));
+    myHeaders.append("Content-Type", "application/json");
 
-fetch("http://52.158.43.53:8080/api/transactions/create", requestOptions)
-  .then((response) => response.text())
-  .then((result) => {
-    window.location.href = "/template";
-  })
-  .catch((error) => console.error(error));
+    const raw = JSON.stringify({
+      account: receiver,
+      TransactionAmount: amount,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+    
+
+    fetch("http://52.158.43.53:8080/api/transactions/create", requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          {
+            return res.status(403).json({
+                error: "Invalid Field, Please enter valid data",
+              });
+          }
+        }
+      })
+      .then((result) => {
+        res.redirect("/template")
+      })
+      .catch((error) => console.error(error));
+  });
 }
+
+
+module.exports = transfer;
