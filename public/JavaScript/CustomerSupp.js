0192fe68bc801e212058291f099847c1c07469d3
Hadyaziz05 <hadyabdelaziz05@gmail.com>
2024-04-27 06:54:40 +0300

    reviews

--- public/JavaScript/CustomerSupp.js
+++ public/JavaScript/CustomerSupp.js
@@ -1,21 +1,67 @@
+window.onload = function () {
+    generateReviewItems();
+    getCookie()
+    const reviewForm = document.getElementById("reviewForm");
+    reviewForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      const reviewContent = document.getElementById("reviewContent").value;
+      const username = getUsername();
+      reviews.push({ username, content: reviewContent });
+      generateReviewItems(reviews);
+      reviewForm.reset();
+    });
+  };
+  
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
+
 function generateReviewItems(reviews) {
-  const reviewContainer = document.getElementById("reviewContainer");
-  reviewContainer.innerHTML = "";
-  reviews.forEach((review) => {
-    const reviewItem = document.createElement("div");
-    reviewItem.classList.add("review-item");
-    reviewItem.innerHTML = `
-        <span class="title"> ${review.username}</span>
-        <div class="review-content"> ${review.content}
-        `;
-    reviewContainer.appendChild(reviewItem);
-  });
+  
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  let getRequestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+  fetch("http://52.158.43.53:8080/api/reviews/fetch", getRequestOptions)
+    .then((response) => response.text())
+    .then((data) => {
+        const obj = JSON.parse(data)
+        const reviewContainer = document.getElementById("reviewContainer");
+        reviewContainer.innerHTML = "";
+        obj.forEach((review) => {
+            const reviewItem = document.createElement("div");
+            reviewItem.classList.add("review-item");
+            reviewItem.innerHTML = `
+                <span class="title"> ${review.name}</span>
+                <div class="review-content"> ${review.review}
+                `;
+            reviewContainer.appendChild(reviewItem);
+          });
+    })
+    .catch((error) => console.error(error));
+    
 }
 
 function save() {
   let review = document.getElementById("reviewContent").value;
   const myHeaders = new Headers();
-  const token = document.cookies.token;
+  const token = getCookie("token");
   myHeaders.append(
     "x-auth-token",
     token
@@ -33,7 +79,24 @@ function save() {
     body: raw,
     redirect: "follow",
   };
-
+  fetch("http://52.158.43.53:8080/api/users/info", {
+        headers: myHeaders
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch user data');
+        }
+        return response.json(); // Parse response body as JSON
+    })
+    .then((data) => {
+        const newRaw = JSON.stringify({
+            "name": data.name,
+            "review": review
+        });
+        requestOptions.body = newRaw;
+        document.getElementById("username").textContent = data.name
+    })
+    .catch((error) => console.error(error));
   fetch("http://52.158.43.53:8080/api/reviews/create", requestOptions)
     .then((response) => response.text())
     .then((result) => console.log(result))
@@ -55,15 +118,3 @@ let requestOptions = {
     .catch((error) => console.error(error));
 ;
 
-window.onload = function () {
-  generateReviewItems(reviews);
-  const reviewForm = document.getElementById("reviewForm");
-  reviewForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const reviewContent = document.getElementById("reviewContent").value;
-    const username = getUsername();
-    reviews.push({ username, content: reviewContent });
-    generateReviewItems(reviews);
-    reviewForm.reset();
-  });
-};
